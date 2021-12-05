57862076934f4dffd30f9e3d2a5be722205f757b
Yana Yanchenko <yana.yanchenko88@yandex.by>
2021-12-05 13:48:39 +0300

    add cors

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const { errors } = require('celebrate');
 const errorsMy = require('./middlewares/errors');
 const NotFoundError = require('./errors/not-found-err');
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('./middlewares/cors-handler');
 
 const mongoose = require('mongoose', {
   useNewUrlParser: true,
@@ -18,12 +20,15 @@ const mestodb = 'mongodb://localhost:27017/mestodb';
 const { PORT = 3000 } = process.env;
 
 app.use(express.json());
+app.use(requestLogger);
+app.use(cors);
 
 app.use('/', userRouters);
 app.use('/', cardRouters);
 
 app.use('*', auth, () => { throw new NotFoundError('Не найдено'); });
 
+app.use(errorLogger);
 app.use(errors());
 app.use(errorsMy);
 
